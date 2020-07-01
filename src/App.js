c8e26440f99701526a9e7ab464933b9709ed0f6e
shaikhaalammmari <shaikhaala@live.com>
2020-07-01 14:41:57 +0300

    reactis my ex husband

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styles from "./styles";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h1 style={styles.text}>Shaikha's TimeLine</h1>
+        <h4 style={styles.text}> Ps: nothing special in my timeline</h4>
+      </div>
+    </div>
+   
+    <div style={styles.list}>
+      <p>19th,Feb 1995 Shaikha came to the world to make your life better</p>
+      <p>31,Aug 2002, My young brother My best friend was born to make my life harder</p>
+      <p>2011 gradyated from high school</p>
+      <p>2011 accpeted in Alied health Scienes Center in Kuwait University</p>
+      <p>2018 FINALLY graduated from AHSC ( honory student)</p>
+      <p>2020 survived COVID19 Quarntaine </p>
+      <p>2020 accpeted in Coded summer bootcamp and react is making me miserable</p>
     </div>
   );
 }
+
+//i suck in react!! need to recap everything again </3
 
 export default App;
---
+++ src/styles.js
@@ -0,0 +1,14 @@
+const styles = {
+  text: {
+    textAlign: "center",
+  },
+
+  list: {
+    // 7g el timeline
+    alignItems: "center",
+    justifyContent: "center",
+    display: "flex",
+  },
+};
+
+export default styles;
