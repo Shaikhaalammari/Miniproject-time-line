6402483e10a10a6fde0e8ff44c6bc4a84a9a55c1
shaikhaalammmari <shaikhaala@live.com>
2020-07-01 17:02:19 +0300

    half done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from "./styles";
+import Events from "./Events";
+import EventsList from "./components/EventsList";
 
 function App() {
   return (
@@ -7,21 +9,14 @@ function App() {
       <div>
         <h1 style={styles.text}>Shaikha's TimeLine</h1>
         <h4 style={styles.text}> Ps: nothing special in my timeline</h4>
+        {EventsList}
       </div>
-    </div>
-   
-    <div style={styles.list}>
-      <p>19th,Feb 1995 Shaikha came to the world to make your life better</p>
-      <p>31,Aug 2002, My young brother My best friend was born to make my life harder</p>
-      <p>2011 gradyated from high school</p>
-      <p>2011 accpeted in Alied health Scienes Center in Kuwait University</p>
-      <p>2018 FINALLY graduated from AHSC ( honory student)</p>
-      <p>2020 survived COVID19 Quarntaine </p>
-      <p>2020 accpeted in Coded summer bootcamp and react is making me miserable</p>
     </div>
   );
 }
 
 //i suck in react!! need to recap everything again </3
+
+export default App;
 
 export default App;
---
+++ src/Events.js
@@ -0,0 +1,42 @@
+import React from "react";
+const Events = [
+  {
+    id: 1,
+    date: "19th,Feb 1995",
+    description: "Shaikha came to the world to make your life better",
+  },
+  {
+    id: 2,
+    date: "31,Aug 2002",
+    description:
+      "My young sbrother My best friend was born to make my life harder",
+  },
+  {
+    id: 3,
+    date: "May, 2012",
+    description: "gradyated from high school",
+  },
+  {
+    id: 4,
+    date: " Aug,2012",
+    description: "accpeted in Alied health Scienes Center in Kuwait University",
+  },
+  {
+    id: 5,
+    date: "May,2018",
+    description: "FINALLY graduated from AHSC ( honory student)",
+  },
+  {
+    id: 6,
+    date: "May 2020",
+    description: " survived COVID19 Quarntaine",
+  },
+  {
+    id: 7,
+    date: "June,2020",
+    description:
+      "accpeted in Coded summer bootcamp and react is making me miserable",
+  },
+];
+
+export default Events;
